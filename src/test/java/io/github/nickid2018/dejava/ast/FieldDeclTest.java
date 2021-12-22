9687668012289285620084a85d008b62bbdac64c
代小呆 <diauweb@live.com>
2021-12-22 15:30:35 +0000

    bug: try VerifyError

--- src/test/java/io/github/nickid2018/dejava/ast/FieldDeclTest.java
+++ src/test/java/io/github/nickid2018/dejava/ast/FieldDeclTest.java
@@ -3,20 +3,15 @@ package io.github.nickid2018.dejava.ast;
 import io.github.nickid2018.dejava.api.*;
 import org.junit.jupiter.api.*;
 
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class FieldDeclTest {
     @Test
     void test() {
-
-        var result = new FieldDecl(Typename.of("int"),
-                List.of(new VarDecl("field0", null, true))
-        ).addModifiers(Modifiers.PUBLIC).toSource();
-        assertTrue(result.contains("field0"));
-        assertTrue(result.contains("[]"));
+        var result = new FieldDecl(Typename.of("int"), "field0", null)
+            .addModifiers(Modifiers.PUBLIC).toSource();
+        assertEquals("public int field0", result);
         System.out.println(result);
     }
     // public int field0[];
